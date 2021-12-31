f46695da3919c1423a078ef61c67d137e0cc51ab
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2021-12-31 20:07:21 +0700

    Update helloworld.component.ts

--- src/app/helloworld/helloworld.component.ts
+++ src/app/helloworld/helloworld.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-helloworld',
-  templateUrl: './helloworld.component.html',
+  templateUrl:'./helloworld.component.html',
   styleUrls: ['./helloworld.component.scss']
 })
 export class HelloworldComponent implements OnInit {
