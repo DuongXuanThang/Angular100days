101725745d551e42ae30c891b2ba3f1ed4ece88b
DuongXuanThang <71684568+DuongXuanThang@users.noreply.github.com>
2022-08-10 08:12:24 +0700

    settingEnvir

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloworldComponent } from './helloworld/helloworld.component';
-import { HiComponent } from './hi/hi.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloworldComponent,
-    HiComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
